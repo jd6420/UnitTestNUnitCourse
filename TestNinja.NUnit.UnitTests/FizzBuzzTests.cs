e519fb305d055254c59a83e5a0e3c3dabcd879e8
Juan David Cadavid Quintero <juandavid.cadavid@globant.com>
2022-04-20 16:35:44 -0500

    Primer ejecricio sección 4

---
+++ TestNinja.NUnit.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnit.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(4, "4")]
+        public void GetOutput(int number, string message)
+        {
+            var result = FizzBuzz.GetOutput(number);
+
+            //Assert.AreEqual(message, result);
+
+            Assert.That(result, Is.EqualTo(message).IgnoreCase);
+        }
+    }
+}
